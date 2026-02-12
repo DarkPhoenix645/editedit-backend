e362c86d4bf0491ba90cd2db155fa0155b06d235
Akshita Sure <sureakshita23@gmail.com>
2026-02-12 20:24:16 +0530

    add auth and middleware

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,19 @@
-import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-class Settings:
-    PROJECT_NAME = "EditEdit Backend"
 
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "supersecret")
-    JWT_ALGORITHM = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "editedit-backend"
+
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+
+    DATABASE_URL: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
 
 settings = Settings()
